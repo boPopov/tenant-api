4ad80be5b43b203cf242f597f133f71ee7fc0d06
Bojan <bojanpopovred@gmail.com>
2025-02-12 01:21:47 +0100

    Database connection was set. Additional basic logic for the api handlers was writen.

--- api/main.go
+++ api/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 	"os"
 
+	"github.com/boPopov/tenant-api/api/database"
 	"github.com/boPopov/tenant-api/api/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+
+	database.ConnectDB()
 
 	app := fiber.New()    //Setting Fiber instance
 	routes.SetRouths(app) //Setting rouths
